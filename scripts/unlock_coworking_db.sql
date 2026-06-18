6e3fbdcb906c57667e5d57e46c7a8aa2e52d97e6
Kristinkass <144677531+Kristinkass@users.noreply.github.com>
2026-06-18 15:49:11 +0300

    Delete scripts directory

--- scripts/unlock_coworking_db.sql
+++
@@ -1,7 +0,0 @@
--- Сброс зависших сессий на базе coworking_123456 (выполнить в pgAdmin / psql один раз).
--- После этого перезапустите приложение.
-
-SELECT pg_terminate_backend(pid)
-FROM pg_stat_activity
-WHERE datname = 'coworking_123456'
-  AND pid <> pg_backend_pid();
